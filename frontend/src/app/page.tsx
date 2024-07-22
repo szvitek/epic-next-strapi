793f54b4992f11fa7bd38e8cc06bbe46b3fa86ef
szvitek <gy.szvitek@gmail.com>
2024-07-22 16:22:07 +0100

    feat(HeroSection): hero section created

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,34 @@
+import HeroSection from '@/components/custom/HeroSection';
+import { flattenAttributes } from '@/lib/utils';
+import qs from 'qs';
+
+const homePageQuery = qs.stringify({
+  populate: {
+    blocks: {
+      populate: {
+        image: {
+          fields: ['url', 'alternativeText'],
+        },
+        link: {
+          populate: true,
+        },
+      },
+    },
+  },
+});
+
 async function getStrapiData(path: string) {
-  const baseUrl = "http://localhost:1337";
+  const baseUrl = 'http://localhost:1337';
+  const url = new URL(path, baseUrl);
+  url.search = homePageQuery;
+
   try {
-    const response = await fetch(baseUrl + path);
+    const response = await fetch(url.href, { cache: 'no-store' });
     const data = await response.json();
-    return data;
+    const flattenedData = flattenAttributes(data);
+
+    console.dir(flattenedData, { depth: null });
+    return flattenedData;
   } catch (error) {
     console.error(error);
   }
@@ -11,13 +36,12 @@ async function getStrapiData(path: string) {
 
 export default async function Home() {
   const strapiData = await getStrapiData('/api/home-page');
-  
-  const { title, description } = strapiData.data.attributes;
-  
+
+  const { title, description, blocks } = strapiData;
+
   return (
-    <main className="container mx-auto py-6">
-      <h1 className="text-5xl font-bold">{title}</h1>
-      <p className="text-xl mt-4">{description}</p>
+    <main>
+      <HeroSection data={blocks[0]} />
     </main>
   );
 }
