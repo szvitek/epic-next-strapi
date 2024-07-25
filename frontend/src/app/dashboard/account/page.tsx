aebc1469e5f2d939d354c2d5af2cda8dd569f254
szvitek <gy.szvitek@gmail.com>
2024-07-25 17:28:02 +0100

    feat(Profile): update user profile

---
+++ frontend/src/app/dashboard/account/page.tsx
@@ -0,0 +1,15 @@
+import { ProfileForm } from '@/components/forms/ProfileForm';
+import { getUserMeLoader } from '@/data/services/get-user-me-loader';
+
+export default async function AccountPage() {
+  const user = await getUserMeLoader();
+  const userData = user.data;
+  const userImage = userData?.image;
+
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-5 gap-4 p-4">
+      AccountPage
+      <ProfileForm data={userData} className='col-span-3' />
+    </div>
+  );
+}
