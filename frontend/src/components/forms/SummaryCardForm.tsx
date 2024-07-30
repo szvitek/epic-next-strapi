2c074e0ceaa5fee6edae91dfe7a4a6080341e433
szvitek <gy.szvitek@gmail.com>
2024-07-30 15:36:37 +0100

    feat(Summary): Implementing Summaries update and delete actions

--- frontend/src/components/forms/SummaryCardForm.tsx
+++ frontend/src/components/forms/SummaryCardForm.tsx
@@ -1,4 +1,8 @@
-// import { updateSummaryAction, deleteSummaryAction } from "@/data/actions/summary-actions";
+'use client';
+import {
+  updateSummaryAction,
+  deleteSummaryAction,
+} from '@/data/actions/summary-actions';
 
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
@@ -14,6 +18,14 @@ import {
 
 import SubmitButton from '@/components/custom/SubmitButton';
 import DeleteButton from '@/components/custom/DeleteButton';
+import { useFormState } from 'react-dom';
+import StrapiErrors from '../custom/StrapiErrors';
+
+const INITIAL_STATE = {
+  strapiErrors: null,
+  data: null,
+  message: null,
+};
 
 export function SummaryCardForm({
   item,
@@ -22,7 +34,16 @@ export function SummaryCardForm({
   readonly item: any;
   readonly className?: string;
 }) {
-  // const deleteSummaryById = deleteSummaryAction.bind(null, item.id);
+  const deleteSummaryById = deleteSummaryAction.bind(null, item.id);
+
+  const [deleteState, deleteAction] = useFormState(
+    deleteSummaryById,
+    INITIAL_STATE
+  );
+  const [updateState, updateAction] = useFormState(
+    updateSummaryAction,
+    INITIAL_STATE
+  );
 
   return (
     <Card className={cn('mb-8 relative h-auto', className)}>
@@ -31,7 +52,7 @@ export function SummaryCardForm({
       </CardHeader>
       <CardContent>
         <div>
-          <form>
+          <form action={updateAction}>
             <Input
               id="title"
               name="title"
@@ -51,12 +72,16 @@ export function SummaryCardForm({
               loadingText="Updating Summary"
             />
           </form>
-          <form>
+          <form action={deleteAction}>
             <DeleteButton className="absolute right-4 top-4 bg-red-700 hover:bg-red-600" />
           </form>
         </div>
       </CardContent>
-      <CardFooter></CardFooter>
+      <CardFooter>
+        <StrapiErrors
+          error={deleteState?.strapiErrors || updateState?.strapiErrors}
+        />
+      </CardFooter>
     </Card>
   );
 }
